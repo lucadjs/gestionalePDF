694c4873f9e5b98f382fd6d855f78d3e37936112
lucadjs <luca.digiacinto@protonmail.com>
2025-06-19 13:05:00 +0200

    V1.4.8

--- copisteria-backend/api/ysqltest.js
+++ copisteria-backend/api/ysqltest.js
@@ -1,4 +1,13 @@
-import mysql from "mysql2";
-export default function handler(req, res) {
-  res.status(200).json({ msg: "mysql2 funziona" });
+import mysql from "mysql2/promise";
+
+export default async function handler(req, res) {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+
+  const [rows] = await connection.execute("SELECT 1 + 1 AS result");
+  res.status(200).json({ result: rows[0].result });
 }
