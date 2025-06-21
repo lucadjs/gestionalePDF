668639b6ac8ce03d2915f121706db60790d4a404
lucadjs <luca.digiacinto@protonmail.com>
2025-06-21 11:47:56 +0200

    V1.5.5

--- copisteria-backend/app.js
+++ copisteria-backend/app.js
@@ -24,7 +24,15 @@ const app = express();
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // frontend React locale
+      "https://gestionale-pdf.vercel.app", // produzione (opzionale, se vuoi anche testare da prod)
+    ],
+    credentials: true, // Solo se usi cookie/autenticazione con credenziali
+  })
+);
 app.use(express.json());
 
 app.use("/api/materiali", materialiRoutes);
